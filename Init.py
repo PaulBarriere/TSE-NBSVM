ecfa4b3a51dd64c24f933b1bbbe73f8abf6fa422
Nico101997 <76113404+Nico101997@users.noreply.github.com>
2020-12-30 16:55:54 +0100

    Update Init.py

--- Init.py
+++ Init.py
@@ -19,3 +19,8 @@ I didn't find a way to create a path to the github repository so I use my data s
 '''
 
 path = 'C:/Users/nicol/OneDrive/fac/M2/S1/Maths for M&D Learning 2/Projet/'
+
+
+## 3. Files
+
+liste_fichier = ['Nokia_6610.txt', 'Apex_AD2600_Progressive_scan_DVD_player.txt', 'Canon_G3.txt', 'Creative_Labs_Nomad_Jukebox_Zen_Xtra_40GB.txt', 'Nikon_coolpix_4300.txt']
