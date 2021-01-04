3dfe1ce996044eeef2d03ba54238ec13bfdc9ce4
PaulBarriere <76106238+PaulBarriere@users.noreply.github.com>
2021-01-04 20:12:34 +0100

    Update Init.py

--- Init.py
+++ Init.py
@@ -13,15 +13,7 @@ from sklearn.feature_extraction.text import *
 from sklearn.metrics import *
 from sklearn.model_selection import *
 
-## 2. Path
 
-'''
-I didn't find a way to create a path to the github repository so I use my data stored localy
-'''
-
-path = 'C:/Users/nicol/OneDrive/fac/M2/S1/Maths for M&D Learning 2/Projet/'
-
-
-## 3. Files
+## 2. Files
 
 liste_fichier = ['Nokia_6610.txt', 'Apex_AD2600_Progressive_scan_DVD_player.txt', 'Canon_G3.txt', 'Creative_Labs_Nomad_Jukebox_Zen_Xtra_40GB.txt', 'Nikon_coolpix_4300.txt']
